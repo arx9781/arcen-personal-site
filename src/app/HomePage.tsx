771153746d77d4c55a257398796948ce297dffb5
arxen <ar006985@gmail.com>
2024-08-30 22:51:34 +0530

    site version 1.0 launch

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -1,17 +1,7 @@
-import { useEffect, useState } from "react";
 import PageLayout from "../layout/PageLayout";
-import SkeletonHeading from "../components/SkeletonLoaders/Skeleton";
 
 const HomePage = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
-
-  return loading ? <SkeletonHeading /> : <PageLayout />;
+  return <PageLayout />;
 };
 
 export default HomePage;
